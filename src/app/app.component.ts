faaed4731b66b956edc777cf17402085f25612c4
guifroes1984 <guilhermefroes26@gmail.com>
2024-05-16 01:08:15 -0300

    Criação o primeiro componente, e declarando o componente no módulo.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'gerenciador de fotos';
-  description = 'São Paulo';
-  url = 'https://upload.wikimedia.org/wikipedia/commons/8/85/S%C3%A3o_Paulo_Futebol_Clube.jpg';
+
 }
---
+++ src/app/foto/foto.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-foto',
+  templateUrl: './foto.component.html'
+})
+export class FotoComponent {
+
+  description = 'São Paulo';
+
+  url = 'https://upload.wikimedia.org/wikipedia/commons/6/6f/Brasao_do_Sao_Paulo_Futebol_Clube.svg';
+}
