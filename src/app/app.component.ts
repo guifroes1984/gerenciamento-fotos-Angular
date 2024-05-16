a449284cb7950d82fa5b89f08dc98904e4d29efe
guifroes1984 <guilhermefroes26@gmail.com>
2024-05-16 00:08:20 -0300

    Start na aplicação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gerenciamento-fotos';
+  description = 'São Paulo';
+  url = 'https://upload.wikimedia.org/wikipedia/commons/6/6f/Brasao_do_Sao_Paulo_Futebol_Clube.svg';
 }
